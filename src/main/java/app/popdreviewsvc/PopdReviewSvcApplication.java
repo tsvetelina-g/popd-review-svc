327d478822d17a79034e2f08da90340b68fb3009
Tsvetelina Gadzhelova <tsvetelina.gadzhelova@gmail.com>
2025-11-14 10:34:21 +0200

    chore: initial Spring Boot project setup

---
+++ src/main/java/app/popdreviewsvc/PopdReviewSvcApplication.java
@@ -0,0 +1,13 @@
+package app.popdreviewsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopdReviewSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopdReviewSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/popdreviewsvc/PopdReviewSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package app.popdreviewsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopdReviewSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
